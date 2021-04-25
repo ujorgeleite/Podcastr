bd580b84a1eb06a14e524517ce195bddd62886d5
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-04-25 13:51:21 -0300

    feat(data): get data from simulated api with SSG concept

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
-import { Header } from '../components/Header'
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
+  console.log(props)
   return (
-  <h1>Index</h1>
-  )
-}
+    <>
+    <h1>index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+    </>
+    )
+  }
+  
+  export async function getStaticProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+    
+    return {
+      props:{ 
+        episodes: data 
+      },
+      revalidate: 60 * 60 * 8,
+    }
+  }
